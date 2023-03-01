57495d6313163ccdabfa48ad9b41f574d735aee0
GGalina <71042425+GGalina@users.noreply.github.com>
2023-03-02 01:24:49 +0400

    Updated 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,11 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-let currentTime = localStorage.getItem('videoplayer-current-time');
-
-player.on('timeupdate', throttle(ResumePlayTime, 1000));
+let currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time') ?? 0);
 
 function ResumePlayTime(play) {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(play.seconds));
 };
+player.on('timeupdate', throttle(ResumePlayTime, 1000));
 
 player.setCurrentTime(currentTime);
